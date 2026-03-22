687a58d2cd497f5976505ee50540bf10c0cb8eaf
Vidhi Challani <vchallan@andrew.cmu.edu>
2026-03-22 10:27:43 -0400

    Initial Commit

---
+++ BioReactor/BioReactorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioReactorApp.swift
+//  BioReactor
+//
+//  Created by Vidhi Challani on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct BioReactorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
